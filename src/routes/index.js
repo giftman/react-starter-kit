17fed433f86b907301c0f044bb59a17039bfbf20
chufan <121551591@qq.com>
2017-01-10 12:17:55 +0800

    add products

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,6 +24,7 @@ export default {
     require('./privacy').default,
     require('./admin').default,
     require('./hireus').default,
+    require('./products').default,
 
     // Wildcard routes, e.g. { path: '*', ... } (must go last)
     require('./notFound').default,
